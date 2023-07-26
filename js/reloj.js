fd2daa04e34f5d911bf2df747b0bb2366f11dbc6
katy-paola <kbarbozab@unicartagena.edu.co>
2023-07-26 14:43:50 -0500

    Correct hand init and add numbers to the watch

--- js/reloj.js
+++ js/reloj.js
@@ -42,39 +42,12 @@ const months = [
 const elementos = {
   mode: document.getElementById("mode"),
   cuerpoDom: document.querySelector("body"),
+  container: document.querySelector(".container"),
   center: document.querySelector(".center"),
   time: document.querySelector(".time"),
   date: document.querySelector(".date"),
 }
 
-//creamos una función para cambiar el modo de la página
-
-let isClicked = false;
-
-function changeMode() {
-  elementos.mode.addEventListener("click", darkMode);
-}
-
-changeMode();
-
-function darkMode(){
-  if(!isClicked){
-    elementos.cuerpoDom.className = "active-dark";
-    elementos.mode.className = "mode active-dark hand-dark";
-    elementos.center.className = "center active-dark hand-dark";
-    hourHand.element.className = "hour active-dark hand-dark";
-    minuteHand.element.className = "minute active-dark hand-dark";
-    console.log(isClicked);
-    isClicked = true;
-  }else{
-    elementos.cuerpoDom.className = "";
-    elementos.mode.className = "mode";
-    elementos.center.className = "center";
-    hourHand.element.className = "hour";
-    minuteHand.element.className = "minute";
-    isClicked = false;
-  }
-}
 
 //creamos una función para inicializar las propiedades de cada objeto
 
@@ -88,12 +61,32 @@ function initHands() {
   rotation2 = 0.1;
 
   element3 = document.querySelector(".second");
-  angle3 = currentTime.getSeconds() * 6;
+  angle3 = currentTime.getSeconds() * 6 + 6;
   rotation3 = 6;
 
   hourHand = new hand(element1, angle1, rotation1);
   minuteHand = new hand(element2, angle2, rotation2);
   secondHand = new hand(element3, angle3, rotation3);
+
+  //creamos un bucle para crear los números del reloj
+  const numbersContainer = document.querySelector(".numbers");
+  const radius = 140; // Radio del círculo que contendrá los números
+  const centerX = 160; // Coordenada X del centro del círculo
+  const centerY = 160; // Coordenada Y del centro del círculo
+
+  for (let i = 1; i <= 12; i++) {
+    const number = document.createElement("div");
+    number.textContent = i;
+    number.classList.add("number");
+    numbersContainer.appendChild(number);
+
+    const angle = ((i - 3) * 30 * Math.PI) / 180; // Ángulo en radianes
+    const x = centerX + radius * Math.cos(angle) - 10;
+    const y = centerY + radius * Math.sin(angle) - 10;
+
+    number.style.left = `${x}px`;
+    number.style.top = `${y}px`;
+  }
 }
 initHands();
 
@@ -104,6 +97,9 @@ function updateClock() {
 
   setInterval(() => {
 
+    //actualizamos la hora actual
+    currentTime = new Date();
+
     //actualizamos el valor de la propiedad element de cada objeto
     hourHand.element.style.transform = `rotate(${hourHand.initialAngle - 90}deg)`;
     minuteHand.element.style.transform = `rotate(${minuteHand.initialAngle - 90}deg)`;
@@ -113,12 +109,53 @@ function updateClock() {
     hourHand.initialAngle += hourHand.rotationAngle;
     minuteHand.initialAngle += minuteHand.rotationAngle;
     secondHand.initialAngle += secondHand.rotationAngle;
+    
+//usar innerHTML para mostrar la hora, día, mes y fecha
+
+elementos.time.innerHTML = `<p>${currentTime.toLocaleTimeString("en-US")}</p>`;
+elementos.date.innerHTML = `<p>${days[currentTime.getDay()]}, ${months[currentTime.getMonth()]} ${currentTime.getDate()}</p>`;
   }, 1000);
 }
 
 updateClock();
 
-//usar innerHTML para mostrar la hora, día, mes y fecha
 
-elementos.time.innerHTML = `<p>${currentTime.toLocaleTimeString("en-US")}</p>`;
-elementos.date.innerHTML = `<p>${days[currentTime.getDay()]}, ${months[currentTime.getMonth()]} ${currentTime.getDate()}</p>`;
+//creamos una función para cambiar el modo de la página
+
+let isClicked = false;
+
+function changeMode() {
+  elementos.mode.addEventListener("click", darkMode);
+}
+
+//agregamos un nuevo elemento al objeto elementos
+elementos.number = document.querySelectorAll(".number");
+console.log(elementos.number);
+
+changeMode();
+
+function darkMode(){
+  if(!isClicked){
+    elementos.cuerpoDom.className = "active-dark";
+    elementos.mode.className = "mode active-dark hand-dark";
+    elementos.container.className = "container bg-dark";
+    elementos.center.className = "center active-dark hand-dark";
+    hourHand.element.className = "hour active-dark hand-dark";
+    minuteHand.element.className = "minute active-dark hand-dark";
+    elementos.number.forEach(element => {
+      element.className = "number active-dark";
+    });
+    isClicked = true;
+  }else{
+    elementos.cuerpoDom.className = "";
+    elementos.mode.className = "mode";
+    elementos.container.className = "container";
+    elementos.center.className = "center";
+    hourHand.element.className = "hour";
+    minuteHand.element.className = "minute";
+    elementos.number.forEach(element => {
+      element.className = "number";
+    });
+    isClicked = false;
+  }
+}
